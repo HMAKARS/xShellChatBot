1526e6e005e7f799c28c8f5da38ba02ba5bc8a10
홍명 <hongbook@hongbook.local>
2025-06-13 12:25:57 +0900

    windows patch

--- chatbot/routing.py
+++ chatbot/routing.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<session_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
-    re_path(r'ws/xshell/(?P<session_name>\w+)/$', consumers.XShellConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<session_id>[^/]+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/xshell/(?P<session_name>[^/]+)/$', consumers.XShellConsumer.as_asgi()),
 ]
